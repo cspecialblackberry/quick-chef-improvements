fcef2bfc84a3713643b234163b5823a216a19b93
Hall Family <charliesamhall@icloud.com>
2024-01-18 17:33:16 -0500

    created weekday table and made option field for favorite recipes

--- public/js/mealPlanner.js
+++ public/js/mealPlanner.js
@@ -1,1 +1,33 @@
-console.log('hi')
+const saturdaySelect = document.querySelector('#saturday-select')
+const mondaySelect = document.querySelector('#monday-select')
+const tuesdaySelect = document.querySelector('#tuesday-select')
+const wednesdaySelect = document.querySelector('#wednesday-select')
+const thursdaySelect = document.querySelector('#thursday-select')
+const fridaySelect = document.querySelector('#friday-select')
+const sundaySelect = document.querySelector('#sunday-select')
+
+let selectArray = [sundaySelect, mondaySelect, tuesdaySelect, wednesdaySelect, thursdaySelect, fridaySelect, saturdaySelect]
+
+const getFavoriteRecipes = async () => {
+    const response = await fetch('/api/recipe');
+    const favRecipesData = await response.json();
+    console.log(favRecipesData)
+}
+
+// getFavoriteRecipes()
+
+const createSelectFields = async (select) => {
+    const response = await fetch('/api/recipe');
+    const data = await response.json();
+    
+    const createOption = (data) => {
+        let option = document.createElement('option')
+        option.setAttribute('value', data.name)
+        option.textContent = data.name
+        select.appendChild(option)
+    }
+
+    data.forEach(createOption)
+}
+
+selectArray.forEach(createSelectFields)

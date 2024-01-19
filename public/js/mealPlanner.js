4f1929aaf2e617b58aefb54fb57d257fed4b4591
Hall Family <charliesamhall@icloud.com>
2024-01-18 19:50:23 -0500

    added functionality to find matching recipe in database

--- public/js/mealPlanner.js
+++ public/js/mealPlanner.js
@@ -6,24 +6,28 @@ const thursdaySelect = document.querySelector('#thursday-select')
 const fridaySelect = document.querySelector('#friday-select')
 const sundaySelect = document.querySelector('#sunday-select')
 
+const sundayButton = document.querySelector('#sunday-button')
+const mondayButton = document.querySelector('#monday-button')
+const tuesdayButton = document.querySelector('#tuesday-button')
+const wednesdayButton = document.querySelector('#wednesday-button')
+const thursdayButton = document.querySelector('#thursday-button')
+const fridayButton = document.querySelector('#friday-button')
+const saturdayButton = document.querySelector('#saturday-button')
+
+
 let selectArray = [sundaySelect, mondaySelect, tuesdaySelect, wednesdaySelect, thursdaySelect, fridaySelect, saturdaySelect]
 
-const getFavoriteRecipes = async () => {
-    const response = await fetch('/api/recipe');
-    const favRecipesData = await response.json();
-    console.log(favRecipesData)
-}
-
-// getFavoriteRecipes()
+let data
 
 const createSelectFields = async (select) => {
     const response = await fetch('/api/recipe');
-    const data = await response.json();
+    data = await response.json();
     
     const createOption = (data) => {
         let option = document.createElement('option')
         option.setAttribute('value', data.name)
         option.textContent = data.name
+        console.log(option)
         select.appendChild(option)
     }
 
@@ -31,3 +35,34 @@ const createSelectFields = async (select) => {
 }
 
 selectArray.forEach(createSelectFields)
+
+let clickedButton
+let selectedRecipeName
+
+const findMatchingSelect = (select) => {
+    if(select.id.includes(clickedButton[0])){
+        return select
+    }
+}
+
+const findSelectedRecipeId = (data) => {
+    if(data.name===selectedRecipeName){
+        return data.recipeId
+    }
+}
+
+const displaySelectedRecipe = (event) => {
+    clickedButton = event.target.id.split('-')
+    let matchingSelect = selectArray.find(findMatchingSelect)
+    selectedRecipeName = matchingSelect.value 
+    console.log(selectedRecipeName)
+    selectedRecipeId = data.find(findSelectedRecipeId)
+}
+
+sundayButton.addEventListener('click', displaySelectedRecipe)
+mondayButton.addEventListener('click', displaySelectedRecipe)
+tuesdayButton.addEventListener('click', displaySelectedRecipe)
+wednesdayButton.addEventListener('click', displaySelectedRecipe)
+thursdayButton.addEventListener('click', displaySelectedRecipe)
+fridayButton.addEventListener('click', displaySelectedRecipe)
+saturdayButton.addEventListener('click', displaySelectedRecipe)
